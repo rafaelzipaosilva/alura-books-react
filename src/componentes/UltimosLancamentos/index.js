e2d57f8611e14d39c8fe185df59444496fcf24ac
Rafael "Zipão" Silva <zipao.silva@gmail.com>
2023-04-18 16:49:06 -0300

    aula5.3

--- src/componentes/UltimosLancamentos/index.js
+++ src/componentes/UltimosLancamentos/index.js
@@ -1,4 +1,5 @@
-import {livros} from './dadosUltimosLancamentos';
+import { Titulo } from '../Titulo';
+import { livros } from './dadosUltimosLancamentos';
 import styled from 'styled-components';
 
 const UltimosLancamentosContainer = styled.section`
@@ -6,16 +7,6 @@ const UltimosLancamentosContainer = styled.section`
     padding-bottom: 20px;
     display: flex;
     flex-direction:column;
-`
-
-const Titulo = styled.h2`
-    width: 100%;
-    padding: 30px 0;
-    background-color: #FFFFFF;
-    color: #EB9B00;
-    font-size: 36px;
-    text-align: center;
-    margin: 0;
 `
 
 const NovosLivrosContainer = styled.div`
@@ -29,7 +20,10 @@ const NovosLivrosContainer = styled.div`
 function UltimosLancamentos() {
     return (
         <UltimosLancamentosContainer>
-            <Titulo>ULTIMOS LANÇAMENTOS</Titulo>
+            <Titulo
+                cor='#EB9B00'
+                tamanhoFonte='36px'
+            >ÚLTIMOS LANÇAMENTOS</Titulo>
             <NovosLivrosContainer>
                 {livros.map(livro => (
                     <img src={livro.src} alt={livro.nome} />
